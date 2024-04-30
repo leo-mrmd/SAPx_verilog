d6c885f5f227ef8240f19225c02b9e8d8f0765b8
Léo Marmande <leo.marmande@gmail.com>
2024-04-30 23:53:29 +0200

    bin file init for ram and gitignore

--- SAP-1/modules/ram.v
+++ SAP-1/modules/ram.v
@@ -6,7 +6,12 @@ module ram(
 
     output[7:0] data_o);
 
-reg[7:0] mem[0:15]; 
+reg[7:0] mem[0:15];
+
+initial begin // Not great from what i've picked up
+	$readmemh("program.bin", mem);
+end
+
 
 always @ (negedge cen_i) begin
     if (!cen_i) begin
